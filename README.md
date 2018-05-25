2910d3cf7d4b566351a2735de8a68e77d8281bbe
Clipsey <TheisCHR@live.dk>
2018-05-25 22:04:52 +0200

    added kugame.d

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ ku is a (work in progress) sudoku game, which only runs as root.
 ku is made with the Polyplex framework, and will only run on *NIX systems.
 
 To run the game, run `sudo ku` after compilation.
+
+You will need SDL2, libvorbisfile, OpenAL and OpenGL (and dependancies) installed and ready.
--- source/app.d
+++ source/app.d
@@ -1,13 +1,17 @@
 import std.stdio;
 import core.sys.posix.sys.types;
 import core.sys.posix.unistd;
+import polyplex.core;
+import polyplex;
+import kugame;
 
-void main()
+void main(string[] args)
 {
 	uid_t id = getuid();
 	if (id == 0) {
 		// TODO: Start game
-		
+		BasicGameLauncher.InitSDL();
+		BasicGameLauncher.LaunchGame(new KuGame(), args);
 	} else {
 		writeln("Sorry, this game only runs as root.\nTry: sudo ku");
 	}
